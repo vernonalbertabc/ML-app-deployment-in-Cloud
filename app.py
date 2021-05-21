2e552afef7611096057711c57f1678660adb9f15
vernon.dsouza123 <vernon.dsouza123@gmail.com>
2021-05-21 08:08:43 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+# References
+# https://www.reddit.com/r/learnpython/comments/j57drt/building_a_web_based_app_using_python_and_flask/
+
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+pickle_in = open('regressor.pkl','rb')
+regressor = pickle.load(pickle_in)
+
+
+@app.route('/')
+def main():
+    return render_template('app.html')
+
+@app.route('/send', methods=['POST'])
+def send(ert):
+    if request.method == 'POST':
+        num1 = request.form['Years']
+        num1 = float(num1)
+        prediction = regressor.predict(np.reshape([num1],(-1,1)))
+
+        
+        return render_template('app.html', sum=prediction[0])
+
+       
+
+if __name__=="__main__":
+    
+   
+    app.run(debug=False)
+
+    
+   
+    
